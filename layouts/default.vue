ce89b726641ceb4c47746d68a6dde8765f66257c
Boohja <matthias@comasu.de>
2023-07-24 23:35:49 +0200

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-red-400">
+    Default Layout
+    <TheSidebar />
+    <div class="p-4 sm:ml-64">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+import TheSidebar from "~/components/layout/TheSidebar.vue";
+
+useHead({
+  bodyAttrs: {
+    class: 'bg-slate-700'
+  }
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    Willkommen!
+  </div>
+</template>
